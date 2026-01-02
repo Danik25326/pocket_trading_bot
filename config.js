77043dd517f633ecb2bee7b88f349bb30506d5a1
Данило <danikrut79@gmail.com>
2026-01-02 14:25:21 +0200

    Change GitHub token in config.js
    Updated GitHub token in configuration.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 window.GH_CONFIG = {
-token: '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',
+token: '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',
 owner: 'Danik25326',
 repo: 'pocket_trading_bot',
 workflowId: 'signals.yml'
