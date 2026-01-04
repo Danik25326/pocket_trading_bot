bbeb1ae164d564e36c541f8c85958b3fa8ec6868
Данило <danikrut79@gmail.com>
2026-01-04 22:59:10 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-window.GH_CONFIG = {
-    owner: 'Danik25326',
-    repo: 'pocket_trading_bot',
-    branch: 'main',
-    baseUrl: 'https://danik25326.github.io/pocket_trading_bot',
-    token: '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' // ← Додайте свій GitHub Token тут!
-};
